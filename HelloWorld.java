aa91ea78d8f76143cc6d9365537ca8eba9ff2fb7
Aishwaryalaxmi <aishwaryalaxmi_k@persistent.com>
2023-08-24 11:59:44 +0530

    different print statements

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+        System.out.println("Hello World");
+        System.out.print("Hello World");
+    }
+}
